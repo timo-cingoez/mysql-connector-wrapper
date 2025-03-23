2dc67d6d92624bbfba14ebdab263a60e11260d94
Timo Cingöz <timo.cingoez@prof4.net>
2025-03-23 16:32:38 +0100

    feat: .env, README

---
+++ README.md
@@ -0,0 +1,3 @@
+# MySQL Connector Wrapper
+
+A simple Python wrapper for the mysql-connector-python package.
--- examples/basic_usage.py
+++ examples/basic_usage.py
@@ -1,6 +1,16 @@
 from mysql_connector_wrapper import Connection
+import os
+from dotenv import load_dotenv
 
-with Connection("127.0.0.1", "root", "root", "3308", "crm_timo") as db:
+load_dotenv()
+
+with Connection(
+    os.getenv("DB_HOST"),
+    os.getenv("DB_USER"),
+    os.getenv("DB_PASSWORD"),
+    os.getenv("DB_PORT"),
+    os.getenv("DB_DATABASE"),
+) as db:
     db.connect()
     with db.cursor() as cursor:
         cursor.execute("SELECT * FROM crm_cfg LIMIT 1;")
